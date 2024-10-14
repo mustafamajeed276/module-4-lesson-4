e25ec0bd80997cc986d62ecc54c54b2c2f62a56e
mustafa276 <mmustafamajeed1@gmail.com>
2024-10-14 13:51:25 +0500

    intro to sets

---
+++ activity1.py
@@ -0,0 +1,14 @@
+myset = {1, 2, 3, 4}
+print(myset)
+myset = {1.0, "hello", (1, 2, 3)}
+print(myset)
+myset = {1, 2, 3, 4, 5, 2, 3}
+print(myset)
+myset = set([1, 2, 3, 4])
+print(myset, "\n")
+numset = {0, 1, 2, 3, 4}
+print("orignal form :")
+print(numset)
+numset.pop()
+print("form after removing first element")
+print(numset)
