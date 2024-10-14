ede44064464b191c473bf13a8e01c1ffb795ed92
mustafa276 <mmustafamajeed1@gmail.com>
2024-10-14 13:59:25 +0500

    union and intersection

---
+++ activty2.py
@@ -0,0 +1,11 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("orignal form of both sets")
+print(setx)
+print(sety)
+print("intersection of both sets : ")
+setz = setx.intersection(sety)
+print(setz)
+print("union of both sets : ")
+setz = setx.union(sety)
+print(setz)
